d044e4489e5092bed4af1e4d71f4da3298dc49f1
Gritja <jonatan.arlock@gritacademy.se>
2020-12-14 08:58:18 +0100

    yuihoi

--- Mediabiblioteket/src/mediabiblioteket/GUI.java
+++ Mediabiblioteket/src/mediabiblioteket/GUI.java
@@ -6,58 +6,53 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
 
 import javax.swing.*;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /** 
  * Huvudfönstret i Bibliotekssystemet som användaren interagerar med för att bl.a. söka efter
  * olika mediatyper och låna en specifik media 
  */
-public class GUI extends JFrame implements ActionListener
+public class GUI extends Application 
 {
 	/**
 	 * Serial number
 	 */
 	private static final long serialVersionUID = 7991560170685136013L;
 
-	JPanel theMainPanel;
-
-	JPanel theNorthPanel;
-	JPanel theNorthButtonPanel;
-
-	JPanel theCenterPanel;
-	JPanel theSouthPanel;
-	JButton borrowButton;
-	JButton infoButton;
-
-	JPanel theWestPanel;
-	// JButton theWestLabel;
-
-	JLabel theLeftColumn;
-	JLabel theRightColumn;
-
-	JTextField theSearchField;
-	JLabel mainMenu;
-	JButton searchButton;
-	JButton searchBorrowedButton;
-
-	JTextArea theTextArea;
-	DefaultListModel theTextAreaModel = new DefaultListModel();
-	JList theTextAreaList;
-	JScrollPane theScroller;
-	
-	
-	// Radio buttons
-	private JPanel theRadioButtonsLabel = new JPanel();
-	
-	private JRadioButton radioAll = new JRadioButton("All");
-	private JRadioButton radioTitle = new JRadioButton("Title");
-	private JRadioButton radioID = new JRadioButton("ID");
-	
-	private ButtonGroup radioButtonGroup = new ButtonGroup();
-	
+	//javafx login/ssn-stuff:
+	Parent root;
+	@FXML
+	private Label lblStatus;
+	@FXML
+	private TextField txtUserName;
+	@FXML
+	private Button loginBtn;
+	//biblioteket fxml
+	@FXML
+	private TextField searchMedia;
+	@FXML
+	private Button searchBtn;
+	@FXML
+	private Button searchBorrowed;
 	
 	LibraryController theController;
 	
@@ -67,110 +62,10 @@ public class GUI extends JFrame implements ActionListener
 	 */
 	public GUI()
 	{
+		
 		theController = new LibraryController(this);
-		
-		String userName = JOptionPane.showInputDialog("Enter SSN(Social Security Number: YYMMDD-XXXX)");
-		login(userName);
-		
-		GridLayout thelayout = new GridLayout(1, 3);
-		thelayout.setHgap(100);
-
-		setLayout(thelayout);
-
-		// The Main Panel
-		theMainPanel = new JPanel(new BorderLayout());
-		theMainPanel.setPreferredSize(new Dimension(500, 900));
-		// The North Panel
-		theNorthPanel = new JPanel(new BorderLayout());
-		theNorthPanel.setOpaque(false);
-		theNorthButtonPanel = new JPanel();
-
-		// The South Panel
-		theSouthPanel = new JPanel();
-		borrowButton = new JButton("Borrow");
-		borrowButton.addActionListener(this);
-		infoButton = new JButton("Info");
-		infoButton.addActionListener(this);
-		theSouthPanel.add(borrowButton);
-		theSouthPanel.add(infoButton);
-
-		// The Left Panel
-		theLeftColumn = new JLabel();
-		theLeftColumn.setOpaque(true);
-		theLeftColumn.setBackground(Color.DARK_GRAY);
-		// theLeftColumn.setPreferredSize(new Dimension(100, 600));
-
-		// The Right Panel
-		theRightColumn = new JLabel();
-		theRightColumn.setOpaque(true);
-		theRightColumn.setBackground(Color.DARK_GRAY);
-		
-		
-		theTextArea = new JTextArea();
-		theTextArea.requestFocus();
-		
-		theTextAreaList = new JList();
-		theTextArea.add(theTextAreaList);
-		theScroller = new JScrollPane(theTextAreaList);
-		
-		theTextAreaList.setModel(theTextAreaModel);
-		
-
-		
-		// The radio panel
-		theRadioButtonsLabel = new JPanel();
-		theRadioButtonsLabel.setLayout(new GridLayout(1,3));
-		
-		radioAll = new JRadioButton("All");
-		radioAll.setSelected(true);
-		radioTitle = new JRadioButton("Title");
-		radioID = new JRadioButton("ID");
-		
-		radioButtonGroup = new ButtonGroup();
-		
-		radioButtonGroup.add(radioAll);
-		radioButtonGroup.add(radioTitle);
-		radioButtonGroup.add(radioID);
-		
-		theRadioButtonsLabel.add(radioAll);
-		theRadioButtonsLabel.add(radioTitle);
-		theRadioButtonsLabel.add(radioID);
-		
-
-		searchButton = new JButton("Search");
-		searchButton.addActionListener(this);
-		
-		searchBorrowedButton = new JButton("Borrowed");
-		searchBorrowedButton.addActionListener(this);
-		
-		theNorthButtonPanel.add(searchButton);
-		theNorthButtonPanel.add(searchBorrowedButton);
-
-		theSearchField = new JTextField();
-		mainMenu = new JLabel("<html><h1>Welcome to the Media Library!");
-
-		theSearchField.setPreferredSize(new Dimension(500, 30));
-
-		theNorthPanel.add(mainMenu, BorderLayout.NORTH);
-		theNorthPanel.add(theSearchField, BorderLayout.CENTER);
-		theNorthPanel.add(theNorthButtonPanel, BorderLayout.EAST);
-		theNorthPanel.add(theRadioButtonsLabel, BorderLayout.SOUTH);
-
-		theMainPanel.add(theNorthPanel, BorderLayout.NORTH);
-		theMainPanel.add(theScroller, BorderLayout.CENTER);
-		
-		theMainPanel.add(theSouthPanel, BorderLayout.SOUTH);
-
-		add(theLeftColumn);
-		add(theMainPanel);
-		add(theRightColumn);
-
-		setMinimumSize(new Dimension(900, 400));
-
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(800, 600);
-		pack();
-		setVisible(true);
+//		String userName = txtUserName.getText();
+		System.out.println("GOGOGO");
 	}
 	
 	/**
@@ -186,11 +81,49 @@ public class GUI extends JFrame implements ActionListener
 			{
 				if(!theController.checkIfBorrowerExist(userName))
 				{
-					JOptionPane.showMessageDialog(null, "Incorrect SSN");
-					userName = JOptionPane.showInputDialog("Enter SSN(Social Security Number: YYMMDD-XXXX)");
+					Alert alert = new Alert(AlertType.WARNING, "Incorrect SSN!", ButtonType.OK);
+					alert.showAndWait();
+					System.out.println("NEIN");
+					//JOptionPane.showMessageDialog(null, "Incorrect SSN");
+					//userName = JOptionPane.showInputDialog("Enter SSN(Social Security Number: YYMMDD-XXXX)");
 				}
 				else
 				{
+					System.out.println("HAI");
+					
+//					 String sceneFile = "/mediabiblioteket/MBL.fxml";
+//					    Parent root = null;
+//					    URL    url  = null;
+//					    try
+//					    {
+//					        url  = getClass().getResource( sceneFile );
+//					        root = FXMLLoader.load( url );
+//					        System.out.println( "  fxmlResource = " + sceneFile );
+//					    }
+//					    catch ( Exception ex )
+//					    {
+//					        System.out.println( "Exception on FXMLLoader.load()" );
+//					        System.out.println( "  * url: " + url );
+//					        System.out.println( "  * " + ex );
+//					        System.out.println( "    ----------------------------------------\n" );
+//					    }
+					
+//					FXMLLoader loader = new FXMLLoader(getClass().getResource("/mediabiblioteket/MBL.fxml"));
+//					loader.setController(new MainController(path));
+					//Pane mainPane = loader.load();
+					try {
+						root = FXMLLoader.load(getClass().getResource("/mediabiblioteket/MBL.fxml"));
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+			        Stage primaryStage = new Stage();
+			        
+					VBox vbox = new VBox(root);
+				    Scene scene = new Scene(vbox);
+			        primaryStage.setScene(scene);
+			        primaryStage.show();
 					break;
 				}
 			
@@ -212,7 +145,7 @@ public class GUI extends JFrame implements ActionListener
 	 */
 	public void clearTheTextArea()
 	{
-		theTextAreaModel.clear();
+		//theTextAreaModel.clear();
 	}
 	
 	/**
@@ -221,7 +154,7 @@ public class GUI extends JFrame implements ActionListener
 	 */
 	public void setTheTextArea(String addText)
 	{
-		theTextAreaModel.addElement(addText);
+		//theTextAreaModel.addElement(addText);
 	}
 	/**
 	 * Lyssnar på Event ifrån användaren.Här kan låntagaren välja att 
@@ -230,111 +163,128 @@ public class GUI extends JFrame implements ActionListener
 	 * 3.Låna ett visst media
 	 * 4.Returnera ett lånat media
 	 **/
-	@Override
 	public void actionPerformed(ActionEvent e)
 	{
-		if(searchButton==e.getSource())
-		{
-			String theInput = theSearchField.getText();
-			if(theController.checkUserInput(theInput))
-			{
-				borrowButton.setText("Borrow");
-				clearTheTextArea();
-				if(radioID.isSelected())
-				{
-					if(theController.checkInputOnlyDigits(theInput))
-					{
-						Media temp = theController.getMedia(theInput);
-						theController.mediaSearchResults.add(temp);
-						if(temp!=null)
-							setTheTextArea(temp.toString());
-					}
-				}
-				else if(radioTitle.isSelected())
-				{
-					theController.searchMediaTitleByString(theInput);
-				}
-				else if(radioAll.isSelected())
-				{
-					theController.searchMediaAllByString(theInput);
-				}
-			}
-			else
-			{
-				JOptionPane.showMessageDialog(null, "The textbox contains incorrect characters");
-			}
-			
-		}
-		else if(infoButton==e.getSource())
-		{
-			String selectedText = (String) theTextAreaList.getSelectedValue();
-			System.out.println(selectedText);
-		if(selectedText!=null)
-			theController.showSelectedMediaInfo(selectedText);
-		}
-		else if(borrowButton==e.getSource())
-		{
-				if(borrowButton.getText().equals("Borrow"))
-				{
-				String selectedText = (String) theTextAreaList.getSelectedValue();
-				
-				Media selectedMedia = theController.getMediaFromSearchResult(selectedText);
-				
-				if(selectedText!=null && selectedMedia!=null)
-				{
-					if(selectedMedia.isBorrowed())
-					{
-						JOptionPane.showMessageDialog(null, "Cannot borrow, already borrowed");
-					}
-					else
-					{
-						theController.borrowMedia(selectedMedia);
-						theTextAreaModel.set(theTextAreaList.getSelectedIndex(), selectedMedia.toString());
-					}
-				}
-				else
-				{
-					System.out.println("Its null");
-				}
-			}
-			else
-			{
-				String selectedText = (String) theTextAreaList.getSelectedValue();
-				Media selectedMedia = theController.getMediaFromSearchResult(selectedText);
-				
-				if(selectedText!=null && selectedMedia!=null)
-				{
-					if(selectedMedia.isBorrowed()==false)
-					{
-						JOptionPane.showMessageDialog(null, "Cannot returm, already returned");
-					}
-					else
-					{
-						theController.returnMedia(selectedMedia);
-						theTextAreaModel.set(theTextAreaList.getSelectedIndex(), selectedMedia.toString());
-					}
-				}
-				
-			}
-			
-			
-		}
-		else if(searchBorrowedButton==e.getSource())
-		{
-			clearTheTextArea();
-			theController.searchBorrowed();
-			borrowButton.setText("Return");
-		}
+		loginBtn.setOnAction((ActionEvent event) -> {
+			login(txtUserName.getText());
+		});
 		
-	}
+//		searchBtn.setOnAction((ActionEvent j) -> {
+//			login(txtUserName.getText());
+//		});
+		
+	}	
+//		if(searchButton==e.getSource())
+//		{
+//			String theInput = theSearchField.getText();
+//			if(theController.checkUserInput(theInput))
+//			{
+//				borrowButton.setText("Borrow");
+//				clearTheTextArea();
+//				if(radioID.isSelected())
+//				{
+//					if(theController.checkInputOnlyDigits(theInput))
+//					{
+//						Media temp = theController.getMedia(theInput);
+//						theController.mediaSearchResults.add(temp);
+//						if(temp!=null)
+//							setTheTextArea(temp.toString());
+//					}
+//				}
+//				else if(radioTitle.isSelected())
+//				{
+//					theController.searchMediaTitleByString(theInput);
+//				}
+//				else if(radioAll.isSelected())
+//				{
+//					theController.searchMediaAllByString(theInput);
+//				}
+//			}
+//			else
+//			{
+//				JOptionPane.showMessageDialog(null, "The textbox contains incorrect characters");
+//			}
+//			
+//		}
+//		else if(infoButton==e.getSource())
+//		{
+//			String selectedText = (String) theTextAreaList.getSelectedValue();
+//			System.out.println(selectedText);
+//		if(selectedText!=null)
+//			theController.showSelectedMediaInfo(selectedText);
+//		}
+//		else if(borrowButton==e.getSource())
+//		{
+//				if(borrowButton.getText().equals("Borrow"))
+//				{
+//				String selectedText = (String) theTextAreaList.getSelectedValue();
+//				
+//				Media selectedMedia = theController.getMediaFromSearchResult(selectedText);
+//				
+//				if(selectedText!=null && selectedMedia!=null)
+//				{
+//					if(selectedMedia.isBorrowed())
+//					{
+//						JOptionPane.showMessageDialog(null, "Cannot borrow, already borrowed");
+//					}
+//					else
+//					{
+//						theController.borrowMedia(selectedMedia);
+//						theTextAreaModel.set(theTextAreaList.getSelectedIndex(), selectedMedia.toString());
+//					}
+//				}
+//				else
+//				{
+//					System.out.println("Its null");
+//				}
+//			}
+//			else
+//			{
+//				String selectedText = (String) theTextAreaList.getSelectedValue();
+//				Media selectedMedia = theController.getMediaFromSearchResult(selectedText);
+//				
+//				if(selectedText!=null && selectedMedia!=null)
+//				{
+//					if(selectedMedia.isBorrowed()==false)
+//					{
+//						JOptionPane.showMessageDialog(null, "Cannot returm, already returned");
+//					}
+//					else
+//					{
+//						theController.returnMedia(selectedMedia);
+//						theTextAreaModel.set(theTextAreaList.getSelectedIndex(), selectedMedia.toString());
+//					}
+//				}
+//				
+//			}
+//			
+//			
+//		}
+//		else if(searchBorrowedButton==e.getSource())
+//		{
+//			clearTheTextArea();
+//			theController.searchBorrowed();
+//			borrowButton.setText("Return");
+//		}
+//		
+//	}
 	/**
 	 * Programstarten som öppnar applikationen
 	 */
-	public static void main(String[] arguments)
+	public static void main(String[] args)
 	{
-		new GUI();
+		launch(args);
 	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		root = FXMLLoader.load(getClass().getResource("/mediabiblioteket/Login.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+		new GUI();
+	}
 
 
 }
